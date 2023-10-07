b921721c6f148544ab173407c5da7141643f0785
Törőcsik Ádám <toarexer@gmail.com>
2023-10-07 19:54:30 +0200

    switched to php and fixed desktop navbar
    Replaced the go web server and html templating with php.
    
    The desktop version of the navbar now has the correct styling, but the hamburger menu version is still wrong.

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Étlap</title>
+    <link rel="stylesheet" href="style/style.css">
+    <script src="script/script.js"></script>
+</head>
+
+<body>
+    <nav id="desktop-nav" class="navbar">
+        <a href="/?day=hetfo">Hétfő</a>
+        <a href="/?day=kedd">Kedd</a>
+        <a href="/?day=szerda">Szerda</a>
+        <a href="/?day=csutortok">Csütörtök</a>
+        <a href="/?day=pentek">Péntek</a>
+        <a href="/?day=szombat">Szombat</a>
+        <a href="/?day=vasarnap">Vasárnap</a>
+    </nav>
+
+    <nav id="mobile-nav" class="navbar">
+        <div id="days">
+            <a href="/?day=hetfo">Hétfő</a>
+            <a href="/?day=kedd">Kedd</a>
+            <a href="/?day=szerda">Szerda</a>
+            <a href="/?day=csutortok">Csütörtök</a>
+            <a href="/?day=pentek">Péntek</a>
+            <a href="/?day=szombat">Szombat</a>
+            <a href="/?day=vasarnap">Vasárnap</a>
+        </div>
+        <a href="javascript:void(0);" class="icon" onclick="ToggleHamburgerMenu()">
+            <i class="fa fa-bars"></i>
+        </a>
+    </nav>
+
+    <main>
+        <?php
+        $query;
+        parse_str(parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY), $query);
+
+        $days = json_decode(file_get_contents("menu-days.json"), true);
+        $cards = json_decode(file_get_contents("menu-cards.json"), true);
+
+        $day = $days[$query['day']];
+
+        echo "<h1>{$day['name']}i étlap</h1>\n<div>\n";
+        foreach ($day['dishes'] as $id) {
+            echo "<div class=\"card\">\n";
+            echo "<img src=\"img/{$cards[$id]['img']}\" alt=\"{$cards[$id]['name']} kép\">\n";
+            echo "<h1>{$cards[$id]['name']}</h1>\n";
+            foreach ($cards[$id]['desc'] as $desc) {
+                echo "<p>{$desc}</p>\n";
+            }
+            echo "</div>\n";
+        }
+        echo "</div>\n";
+        ?>
+    </main>
+</body>
+
+</html>
--- server/go.mod
+++
@@ -1,3 +0,0 @@
-module ikt-etlap/server
-
-go 1.21.1
