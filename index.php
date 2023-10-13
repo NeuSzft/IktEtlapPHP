b3d7401ab6adbead31208ebe14d8c2744432df3e
Törőcsik Ádám <toarexer@gmail.com>
2023-10-13 12:01:34 +0200

    Fixed URL parsing

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 
-$path = $_SERVER['REQUEST_URI'];
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 function route(string $file, string $url)
 {
+    var_dump($url);
     if (!include('./pages/' . $file)) {
         echo "404 Not Found '$url'";
         http_response_code(404);
